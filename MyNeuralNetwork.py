49dc5216243d917ae66030501b01145798b254d1
EquiPao2201 <lozada.m.erika@gmail.com>
2023-11-12 20:22:15 -0500

    three methods

--- MyNeuralNetwork.py
+++ MyNeuralNetwork.py
@@ -14,6 +14,20 @@ class MyNeuralNetwork:
     self.w.append(np.zeros((1, 1)))
     for lay in range(1, self.L):
       self.w.append(np.zeros((layers[lay], layers[lay - 1])))
+  
+  def fit(X, y):
+    """Train the network using backpropagation""" 
+    """X-> array (n_samples,n_features), which holds the training samples represented as floating point feature vectors"""
+    """y->a vector of size (n_samples), which holds the target values (class labels) for the training samples"""
+  
+  def  predict(X):
+    """Predict output from input X"""
+    """X-> array of size (n_samples,n_features) that contains the samples. This method returns a vector with the predicted values for all the input samples"""
+  
+  def loss_epochs():
+    """Calculate and return average loss over epochs""" 
+    """returns 2 arrays of size (n_epochs, 2) that contain the evolution of the training error and the validation error for each of the epochs of the system, so this information can be plotted"""
+    
 
 # layers include input layer + hidden layers + output layer
 layers = [4, 9, 5, 1]
