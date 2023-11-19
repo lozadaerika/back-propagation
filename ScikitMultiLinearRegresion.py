492104e9bdfa447b28892584c9f3f535e76725e4
EquiPao2201 <lozada.m.erika@gmail.com>
2023-11-18 22:17:23 -0500

    Multi linear regresion on Sci-kit learn

---
+++ ScikitMultiLinearRegresion.py
@@ -0,0 +1,34 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+# Read input and target data from file
+file_content =  np.loadtxt('A1-synthetic.txt')
+
+input_columns=4
+output_column=5
+test_percentage=0.15
+validation_percentage=0.25
+activation_function='sigmoid'
+learning_rate=0.01
+momentum=0.9
+epochs=1000
+
+input_data = file_content[:, :input_columns]
+target_data = file_content[output_column]
+
+# Split the data into training and test sets
+input_train_val, input_test, target_train_val, target_test = train_test_split(input_data, target_data, test_size=test_percentage, random_state=42)
+# Split the training and validation sets
+input_train, input_val, target_train, target_val = train_test_split(input_train_val, target_train_val, test_size=validation_percentage, random_state=42)
+
+# Scikit-learn's multi-linear regression
+model = LinearRegression()
+
+# Fit the model
+model.fit(input_train, target_train)
+
+# Evaluate model on the test set
+test_predictions = model.predict(input_test)
+print("Test Predictions (Scikit-learn Linear Regression):", test_predictions)
